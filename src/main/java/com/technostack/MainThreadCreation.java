7d7f4c08eb6f679287c6cf05b97e3ff8d16019dc
Bikash Mohapatra <tech.mbikash@gmail.com>
2025-05-29 20:13:27 +0530

    Main Thread printed

---
+++ src/main/java/com/technostack/MainThreadCreation.java
@@ -0,0 +1,7 @@
+package com.technostack;
+
+public class MainThreadCreation {
+    public static void main(String[] args) {
+        System.out.println("Thread Name :"+ Thread.currentThread().getName());
+    }
+}
